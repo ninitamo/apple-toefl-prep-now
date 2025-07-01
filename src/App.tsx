88ad145a640466a57bdb4d0f1f1794469d2f0b18
Nino <n.kezherashvili@twinit.ge>
2025-07-01 17:17:08 +0400

    toefl-project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 
+import React, { useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import Index from "./pages/Index";
@@ -30,83 +31,99 @@ import SpeakingPractice from "./pages/SpeakingPractice";
 import WritingPracticeList from "./pages/WritingPracticeList";
 import WritingPractice from "./pages/WritingPractice";
 import NotFound from "./pages/NotFound";
-
+import ReactGA from "react-ga4";
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/" element={<Index />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/exam-info" element={<ExamInfo />} />
-            <Route path="/institution-search" element={<InstitutionSearch />} />
-            <Route path="/blog/why-toefl-exam" element={<WhyToeflBlog />} />
-            <Route path="/blog/who-needs-toefl" element={<WhoNeedsToeflBlog />} />
-            <Route path="/blog/which-toefl-test" element={<WhichToeflTestBlog />} />
-            <Route path="/blog/where-toefl-accepted" element={<WhereToeflAcceptedBlog />} />
-            <Route path="/blog/when-toefl-conducted" element={<WhenToeflConductedBlog />} />
-            <Route path="/blog/what-toefl-stands-for" element={<WhatToeflStandsForBlog />} />
-            <Route path="/blog/how-toefl-scored" element={<HowToeflScoredBlog />} />
-            <Route path="/practice/full-tests" element={<FullTestsList />} />
-            <Route path="/practice/sections" element={<IndividualSectionsPage />} />
-            <Route path="/test/:testId" element={<ToeflTest />} />
-            <Route path="/practice/reading" element={
-              <ProtectedRoute>
-                <ReadingPracticeList />
-              </ProtectedRoute>
-            } />
-            <Route path="/practice/reading/urbanization" element={
-              <ProtectedRoute>
-                <ReadingPractice />
-              </ProtectedRoute>
-            } />
-            <Route path="/practice/listening" element={
-              <ProtectedRoute>
-                <ListeningPracticeList />
-              </ProtectedRoute>
-            } />
-            <Route path="/practice/listening/library-resources" element={
-              <ProtectedRoute>
-                <ListeningPractice />
-              </ProtectedRoute>
-            } />
-            <Route path="/practice/speaking" element={
-              <ProtectedRoute>
-                <SpeakingPracticeList />
-              </ProtectedRoute>
-            } />
-            <Route path="/practice/speaking/:practiceId" element={
-              <ProtectedRoute>
-                <SpeakingPractice />
-              </ProtectedRoute>
-            } />
-            <Route path="/practice/writing" element={
-              <ProtectedRoute>
-                <WritingPracticeList />
-              </ProtectedRoute>
-            } />
-            <Route path="/practice/writing/exam-mode" element={
-              <ProtectedRoute>
-                <WritingPractice />
-              </ProtectedRoute>
-            } />
-            <Route path="/practice/writing/:practiceId" element={
-              <ProtectedRoute>
-                <WritingPractice />
-              </ProtectedRoute>
-            } />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </AuthProvider>
-  </QueryClientProvider>
-);
+const AnalyticsListener: React.FC = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
+  return null;
+};
+
+
+const App = () => {
+  ReactGA.initialize("G-325NCBYMCY")
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <AnalyticsListener />
+            <Routes>
+              <Route path="/auth" element={<Auth />} />
+              <Route path="/" element={<Index />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/exam-info" element={<ExamInfo />} />
+              <Route path="/institution-search" element={<InstitutionSearch />} />
+              <Route path="/blog/why-toefl-exam" element={<WhyToeflBlog />} />
+              <Route path="/blog/who-needs-toefl" element={<WhoNeedsToeflBlog />} />
+              <Route path="/blog/which-toefl-test" element={<WhichToeflTestBlog />} />
+              <Route path="/blog/where-toefl-accepted" element={<WhereToeflAcceptedBlog />} />
+              <Route path="/blog/when-toefl-conducted" element={<WhenToeflConductedBlog />} />
+              <Route path="/blog/what-toefl-stands-for" element={<WhatToeflStandsForBlog />} />
+              <Route path="/blog/how-toefl-scored" element={<HowToeflScoredBlog />} />
+              <Route path="/practice/full-tests" element={<FullTestsList />} />
+              <Route path="/practice/sections" element={<IndividualSectionsPage />} />
+              <Route path="/test/:testId" element={<ToeflTest />} />
+              <Route path="/practice/reading" element={
+                <ProtectedRoute>
+                  <ReadingPracticeList />
+                </ProtectedRoute>
+              } />
+              <Route path="/practice/reading/urbanization" element={
+                <ProtectedRoute>
+                  <ReadingPractice />
+                </ProtectedRoute>
+              } />
+              <Route path="/practice/listening" element={
+                <ProtectedRoute>
+                  <ListeningPracticeList />
+                </ProtectedRoute>
+              } />
+              <Route path="/practice/listening/library-resources" element={
+                <ProtectedRoute>
+                  <ListeningPractice />
+                </ProtectedRoute>
+              } />
+              <Route path="/practice/speaking" element={
+                <ProtectedRoute>
+                  <SpeakingPracticeList />
+                </ProtectedRoute>
+              } />
+              <Route path="/practice/speaking/:practiceId" element={
+                <ProtectedRoute>
+                  <SpeakingPractice />
+                </ProtectedRoute>
+              } />
+              <Route path="/practice/writing" element={
+                <ProtectedRoute>
+                  <WritingPracticeList />
+                </ProtectedRoute>
+              } />
+              <Route path="/practice/writing/exam-mode" element={
+                <ProtectedRoute>
+                  <WritingPractice />
+                </ProtectedRoute>
+              } />
+              <Route path="/practice/writing/:practiceId" element={
+                <ProtectedRoute>
+                  <WritingPractice />
+                </ProtectedRoute>
+              } />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+  )
+}
 
 export default App;
